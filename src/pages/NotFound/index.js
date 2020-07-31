bfb737d222f77f247d7323c3587ac51fed4fd888
Antonio Sergio Jr <sergio.gxp@gmail.com>
2020-07-30 22:48:32 -0300

    Aula 02

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo404 from "../../assets/img/Logo404.png";
+import Footer from "../../components/Footer";
+import Menu from "../../components/Menu";
+
+const NotFound = () => (
+  <React.Fragment>
+    <Menu />
+    <div style={{ textAlign: "center" }}>
+      <Link to="/">
+        <img
+          className="Logo404"
+          src={Logo404}
+          alt="AntonioFlix Logo404"
+          style={{ width: "50%" }}
+        />
+      </Link>
+      <p style={{ fontSize: "50px" }}>Ops...página não encontrada!</p>
+    </div>
+    <Footer />
+  </React.Fragment>
+);
+
+export default NotFound;
